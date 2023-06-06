80a6afe1a91cf99cf8e5e6d9352ef6ed6946ab08
ForamGhoniya <foramghoniya2002@gmail.com>
2023-06-06 14:15:51 +0530

    push all changes regarding to weather design

--- features/weatherapp.js
+++ features/weatherapp.js
@@ -1,28 +1,29 @@
-// function updateTime() {
-//     let date = new Date();
-//     let dates = date.getDate();
-//     const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//     let name = month[date.getMonth()];
-//     const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-//     const day = days[date.getDay()];
+function updateTime() {
+    let date = new Date();
+    let dates = date.getDate();
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let name = month[date.getMonth()];
+    const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    const day = days[date.getDay()];
 
 
-//     document.getElementById('text').innerHTML = dates + " " + name + ", " + day + " ";
+    document.getElementById('display-date').innerHTML = dates + " " + name + ", " + day + " ";
 
-// }
-// setInterval(updateTime, 1000);
+}
+setInterval(updateTime, 1000);
+
 let search = document.querySelector(".searchIcon");
 
 function fetchData() {
     let city = document.getElementById("cityName").value;
     console.log(city);
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b6a20c093d78b4573edf3f989d9c56f5`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b6a20c093d78b4573edf3f989d9c56f5&units=metric`)
         .then((response) => response.json())
 
         .then((response) => {
             console.log(response)
-            document.getElementById("display-temp").innerHTML = `${response.main.temp}°C`;
+            document.getElementById("display-temp").innerHTML = `${parseInt(response.main.temp.toString())}°C`;
             document.getElementById("display-humidity").innerHTML = `${response.main.humidity}%`;
             document.getElementById("wind-speed").innerHTML = `${response.wind.speed}km/h`;
             document.getElementById("weather-desc").innerHTML = `${response.weather[0].description}`;
