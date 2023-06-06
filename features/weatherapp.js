335e148d7877a43d57cce3c4510dd33d3ea98a78
ForamGhoniya <foramghoniya2002@gmail.com>
2023-06-06 09:53:30 +0530

    comlete weather design

---
+++ features/weatherapp.js
@@ -0,0 +1,37 @@
+// function updateTime() {
+//     let date = new Date();
+//     let dates = date.getDate();
+//     const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//     let name = month[date.getMonth()];
+//     const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+//     const day = days[date.getDay()];
+
+
+//     document.getElementById('text').innerHTML = dates + " " + name + ", " + day + " ";
+
+// }
+// setInterval(updateTime, 1000);
+let search = document.querySelector(".searchIcon");
+
+function fetchData() {
+    let city = document.getElementById("cityName").value;
+    console.log(city);
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b6a20c093d78b4573edf3f989d9c56f5`)
+        .then((response) => response.json())
+
+        .then((response) => {
+            console.log(response)
+            document.getElementById("display-temp").innerHTML = `${response.main.temp}°C`;
+            document.getElementById("display-humidity").innerHTML = `${response.main.humidity}%`;
+            document.getElementById("wind-speed").innerHTML = `${response.wind.speed}km/h`;
+            document.getElementById("weather-desc").innerHTML = `${response.weather[0].description}`;
+            document.getElementById("weather-main").innerHTML = `${response.weather[0].main}`;
+        }
+        );
+}
+search.addEventListener("click", () => {
+    fetchData()
+});
+
+
