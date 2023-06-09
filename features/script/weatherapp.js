39ae8b27f39284f6be2ccbce53eb19ca238f7e26
ForamGhoniya <foramghoniya2002@gmail.com>
2023-06-09 16:18:29 +0530

    change image according wether

--- features/script/weatherapp.js
+++ features/script/weatherapp.js
@@ -25,11 +25,13 @@ function fetchData() {
             console.log(response)
 
             document.getElementById("city").innerHTML = `${response.name}`;
+
             document.getElementById("country").innerHTML = `${response.sys.country}`;
 
             document.getElementById("temp--weather").innerHTML = `${parseInt(response.main.temp.toString())}°C`;
 
             document.getElementById("weather-desc").innerHTML = `${response.weather[0].description}`;
+
             document.getElementById("display-humidity").innerHTML = `${response.main.humidity}%`;
 
             document.getElementById("wind-speed").innerHTML = `${response.wind.speed}km/h`;
@@ -39,9 +41,37 @@ function fetchData() {
             document.getElementById("lat").innerHTML = `${response.coord.lat}`;
 
             document.getElementById("lon").innerHTML = `${response.coord.lon}`;
+            changeImage(response.weather[0].description);
         }
         );
 }
+
+function changeImage(weatherDescription) {
+
+    const weatherImages = {
+        ['scattered clouds']: '../assets/images/scattered-clouds.png',
+        ['few clouds']: '../assets/images/few-clouds.png',
+        ['broken clouds']: '../assets/images/broken-clouds.png',
+        ['overcast clouds']: '../assets/images/33.gif',
+        ['smoke']: '../assets/images/i',
+        ['clear sky']: '../assets/images/11.gif',
+        ['LightRain']: '../assets/images/44.gif',
+        ['haze']: '../assets/images/haze.png',
+    };
+
+    const imgElement = document.getElementById('weather-image');
+
+
+    if (weatherImages) {
+
+        const imageUrl = weatherImages[weatherDescription];
+
+        imgElement.src = imageUrl;
+    } else {
+        imgElement.src = '../assets/image/sunn.gif';
+    }
+}
+
 var input = document.getElementById("cityName");
 input.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
@@ -52,5 +82,4 @@ input.addEventListener("keypress", function (event) {
 search.addEventListener("click", () => {
     fetchData()
 });
-
 
